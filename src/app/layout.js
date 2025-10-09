a94d88c9012c701808ad8b2b9a373ebc06bb0e24
SHIVANI PRAJAPATI <shivaniprajapati10jan@gmail.com>
2025-10-09 19:37:24 +0530

    added animated backgorund with UI enhance

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import Navbar from './pages/Navbar'
-import '../styles/globals.css'
-import { ThemeProvider } from './context/ThemeContext'
-import { AnalyticsProvider } from './context/AnalyticsContext'
-import Footer from './pages/Footer';
+import Navbar from "./pages/Navbar";
+import "../styles/globals.css";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AnalyticsProvider } from "./context/AnalyticsContext";
+import Footer from "./pages/Footer";
 
 export const metadata = {
-  title: 'Open Source Component Library',
-  description: 'Demo of reusable components (Buttons & Cards)'
-}
+  title: "Open Source Component Library",
+  description: "Demo of reusable components (Buttons & Cards)",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -15,16 +15,24 @@ export default function RootLayout({ children }) {
       <body className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
         <ThemeProvider>
           <AnalyticsProvider>
-            <div className="">
+            <div className="relative">
               <Navbar />
-              <main className='max-w-7xl mx-auto'>
-                {children}
-              </main>
+              {/* animated backgound */}
+              <div className="fixed inset-0 top-1/3 left-1/3 -translate-x-1/2 -translate-y-1/2">
+                 <div className="absolute top-14 left-1/5 w-32 h-32 rounded-full bg-gradient-to-r from-purple-500 to-pink-500  opacity-70 animation-pulseBg"></div>
+              </div>
+              <div className="fixed inset-0 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                 <div className="absolute top-84 left-2/5 w-28 h-28 rounded-full bg-gradient-to-r from-purple-500 to-pink-500  opacity-70 animation-pulseBg"></div>
+              </div>
+              <div className="fixed inset-0 top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                 <div className="absolute top-12 left-10/12 w-28 h-28 rounded-full bg-gradient-to-r from-purple-500 to-pink-500  opacity-70 animation-pulseBg"></div>
+              </div>
+              <main className="max-w-7xl mx-auto">{children}</main>
               <Footer />
             </div>
           </AnalyticsProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
